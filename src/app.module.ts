655834555b1765e84ea10969a8f26934a0093310
Vinicius Conti <vinicius.sconti@eaportal.org>
2026-08-09 09:43:05 -0300

    Criação do banco de dados postgreSQL, instalação do PrismaORM e integração do banco com o projeto usando a migration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UsersController } from './users/users.controller';
 import { UsersService } from './users/users.service';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [],
+  imports: [PrismaModule],
   controllers: [UsersController],
   providers: [UsersService],
 })
